7541a8953927fc6d3e34cf8b44c0c9d985efb4f8
Paulo Penicheiro <smasher35@gmail.com>
2017-01-02 19:14:09 +0000

    top tem a 98%, falta só incrementar o valorda posição do jogador que ainda está hardcoded, de resto já vai  buscar os dados à API

--- 2 - Game/angular/app/topten/topten.component.ts
+++ 2 - Game/angular/app/topten/topten.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+
+import { Component, Input, OnInit } from '@angular/core';
+import {WebSocketService } from '../notifications/websocket.service';
+import { Http, Response, Headers, RequestOptions, RequestOptionsArgs } from '@angular/http';
+
 
 import { FormsModule } from '@angular/forms';
 
@@ -7,4 +11,33 @@ import { FormsModule } from '@angular/forms';
     selector: 'my-app',
     templateUrl: `topten.component.html`
 })
-export class TopTenComponent { }
+export class TopTenComponent {
+    public highScores: any []= [];
+
+    constructor (public http: Http){
+    this.getHighScores();
+    }
+
+    getHighScores () {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http.get('http://localhost:7777/api/v1/top10', <RequestOptionsArgs>{ headers: headers, withCredentials: false})
+        .subscribe(
+            response => {
+                this.highScores = response.json();
+            }, 
+            error => {
+                alert(error.text());
+                console.log(error.text());
+            }
+        );
+    }
+
+
+
+
+}
+ 
+
+ 
